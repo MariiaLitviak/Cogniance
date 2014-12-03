7df75f8f0a8e57a9781cd2653ac995c19acc65a3
Mariia Litviak <malitvia@gmail.com>
2014-12-03 12:49:19 +0200

    changes revie

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-Cogniance
+Cogniance QA task
 =========
 
-Cogniance qa task
+Required libraries to run script:
+- unittest
+- requests
+- json
+- random
+
+To run script perform command: 
+python checkserver.py -v
+
+Script makes 7 tests:
+1) test_gives_list (Checks that the server gives a list of all candidates)
+2) test_add_new_candidate (Checks that the server adds new candidate.)
+3) test_incorrect_add_data (Checks negative verifications)
+4) test_get_one_candidate (Gets the candidate)
+5) test_get_one_wrong_candidat (Gets candidate with wrong id)
+6) test_get_one_wrong_string_candidate (Gets candidate with wrong id as a string)
+7) test_delete_candidate (Checks for correct delete the candidate)
+
--- checkserver.py
+++ checkserver.py
@@ -1,53 +1,85 @@
+import unittest
 import requests
 import json
-from StringIO import StringIO
+import random
 
-print("1. Check that the server gives a list of all candidates.")
-d = requests.get('http://qainterview.cogniance.com/candidates')
-code = d.status_code
-obj = json.loads(d.text)
-if code != 200:
-    print ('FAIL. Return ' + str(code) + ' code in response')
+class TestServer(unittest.TestCase):
 
-size = len(obj['candidates'])
-if size > 0:
-    print ('OK. Return ' + str(size) + ' candidates')
-else:
-    print ('FAIL. Return' + str(size) + ' candidates')
+    # Check that the server gives a list of all candidates.
+    def test_gives_list(self):
+        d = requests.get('http://qainterview.cogniance.com/candidates')
+        code = d.status_code
+        obj = json.loads(d.text)
+        self.assertEqual(code, 200)
 
-print("2. Check that the server add new candidate.")
-load = {'name': 'Masha', 'position': 'QA intern'}
-json_params = json.dumps(load)
-headers = {'content-type': 'application/json'}
-r = requests.post('http://qainterview.cogniance.com/candidates', data=json_params, headers=headers)
-code = r.status_code
-print (r.text)
-obj = json.loads(r.text)
-size = len(['id'])
-if size != 1:
-    print ('FAIL. Return ' + str(size) + ' new candidates')
+        size = len(obj['candidates'])
+        self.assertTrue(size > 0)
 
-new_candidate = obj['candidate']
-new_candidat_id = new_candidate['id']
+    # Check that the server add new candidate.
+    def test_add_new_candidate(self):
+        load = {'name': 'Masha', 'position': 'QA intern'}
+        json_params = json.dumps(load)
+        headers = {'content-test_gives_listtype': 'application/json'}
+        r = requests.post('http://qainterview.cogniance.com/candidates', data=json_params, headers=headers)
+        code = r.status_code
+        #obj = json.loads(r.text)
+        self.assertEqual(code, 201)
+        size = len(['id'])
+        self.assertTrue(size == 1)
 
-if code != 201:
-    print ('FAIL. Return ' + str(code) + ' code in response')
+    # Check negative verifications.
+    def test_incorrect_add_data(self):
+        load = {'dogName': 'Bill', 'catName': 'Sarah', 'parrotName': 'Kluv'}
+        json_params = json.dumps(load)
+        headers = {'content-type': 'application/json'}
+        b = requests.post('http://qainterview.cogniance.com/candidates', data=json_params, headers=headers)
+        code = b.status_code
+        self.assertEqual(code, 400)
 
+    # get the candidate.
+    def test_get_one_candidate(self):
+        load = {'name': 'Masha3', 'position': 'test position'}
+        json_params = json.dumps(load)
+        headers = {'content-type': 'application/json'}
+        rs = requests.post('http://qainterview.cogniance.com/candidates', data=json_params, headers=headers)
+        obj = json.loads(rs.text)
+        new_candidate = obj['candidate']
+        new_candidate_id = new_candidate['id']
 
-print("3. Check negative verifications.")
-incload = {'dogName': 'Bill', 'catName': 'Sarah', 'parrotName': 'Kluv'}
-json_incparams = json.dumps(incload)
-headerstwo = {'content-type': 'application/json'}
-b = requests.post('http://qainterview.cogniance.com/candidates', data=json_incparams, headers=headerstwo)
-code = b.status_code
-if code != 201:
-    print ('OK. Return ' + str(code) + ' code in response with negative verifications')
+        url = ('http://qainterview.cogniance.com/candidates/'+str(new_candidate_id))
+        k = requests.get(url)
+        self.assertEqual(k.status_code, 200)
+        obj = json.loads(k.text)
 
-print("4. Delete the candidat.")
-url = ('http://qainterview.cogniance.com/candidates/'+str(new_candidat_id))
-response = requests.delete(url, data=json.dumps(load), headers=headers)
-k = requests.get('http://qainterview.cogniance.com/candidates')
-code = k.status_code
-if code != 200:
-    print ('FAIL. Return ' + str(code) + ' code in response')
+        self.assertEqual(obj['candidate']['id'], new_candidate_id, 'Returns incorrect id')
+        self.assertEqual(obj['candidate']['name'], 'Masha3', 'Returns incorrect name')
+        self.assertEqual(obj['candidate']['position'], 'test position', 'Returns incorrect position')
 
+    # get wrong candidate with id
+    def test_get_one_wrong_candidate(self):
+        url = ('http://qainterview.cogniance.com/candidates/'+str(random.randint(9999, 99999)))
+        k = requests.get(url)
+        self.assertEqual(k.status_code, 404)
+
+    # get wrong candidate with id as string
+    def test_get_one_wrong_string_candidate(self):
+        url = ('http://qainterview.cogniance.com/candidates/'+'WrongPath')
+        k = requests.get(url)
+        self.assertEqual(k.status_code, 404)
+
+    # Delete the candidat.
+    def test_delete_candidate(self):
+        load = {'name': 'Masha', 'position': 'QA intern'}
+        json_params = json.dumps(load)
+        headers = {'content-type': 'application/json'}
+        rs = requests.post('http://qainterview.cogniance.com/candidates', data=json_params, headers=headers)
+        obj = json.loads(rs.text)
+        new_candidate = obj['candidate']
+        new_candidate_id = new_candidate['id']
+        url = ('http://qainterview.cogniance.com/candidates/'+str(new_candidate_id))
+        k = requests.delete(url, data=json.dumps(load), headers=headers)
+        code = k.status_code
+        self.assertEqual(code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
